8b587e3f33c74164917e8049a7061b7c2ed5609b
Grover Cristobal <dfrontend@medialabla.com>
2023-11-26 00:37:55 -0500

    feat(projects/00-testimonials): mostrar componente

---
+++ projects/00-testimonials/src/App.jsx
@@ -0,0 +1,11 @@
+import Testimony from "./components/Testimony";
+
+function App () {
+    return (
+        <div className="App">
+            <Testimony />
+        </div>
+    )
+}
+
+export default App
