92fea88e08e3ac7cbd3429263e8e67925c717f0c
Grover Cristobal <dfrontend@medialabla.com>
2023-11-25 23:43:32 -0500

    feat(projects/00-testimonials): estructura principal de Testimony

--- projects/00-testimonials/src/components/Testimony.jsx
+++ projects/00-testimonials/src/components/Testimony.jsx
@@ -5,8 +5,18 @@ function Testimony() {
             {/* todo lo q va entre {} se considera un valor o una expresión de JS */}
             <img
                 className='testimony-image'
-                src={require('../images/testimonio-emma.png')}
-                alt='Foto de Emma' />
+                src={require('../images/testimonio-emma.png')} // para insertar una imagen, una opción es usar require()
+                alt='Foto de Emma'
+            />
+            {/* por ahora estamos asignando valores fijos pero luego con las props esto será dinámico para reutilizar código y no tener q
+            escribir uno x cada uno */}
+            <div className='text-testimony-container'>
+                <p className='testimony-name'>Emma Bostian en Suecia</p>
+                <p className='testimony-position'>Ingeniera de Software en Spotify</p>
+                <p className='testimony-text'>Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el 
+                que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba 
+                para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.</p>
+            </div>
         </div>
     )
 }
