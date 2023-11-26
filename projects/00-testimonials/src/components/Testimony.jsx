d47d8c84fb5d77ef305fd42ecf5200ae66d98c33
Grover Cristobal <dfrontend@medialabla.com>
2023-11-26 02:24:50 -0500

    feat(projects/00-testimonials): recibir props en un Componente

--- projects/00-testimonials/src/components/Testimony.jsx
+++ projects/00-testimonials/src/components/Testimony.jsx
@@ -1,7 +1,7 @@
 import EmmaImage from '../images/testimonio-emma.png';
 import '../styles/Testimony.css'
 
-function Testimony() {
+function Testimony(props) {
     // Un componente funcional de React retorna un elemento JSX
     return (
         <div className='testimony-container'>
@@ -14,11 +14,9 @@ function Testimony() {
             {/* por ahora estamos asignando valores fijos pero luego con las props esto será dinámico para reutilizar código y no tener q
             escribir uno x cada uno */}
             <div className='text-testimony-container'>
-                <p className='testimony-name'>Emma Bostian en Suecia</p>
-                <p className='testimony-position'>Ingeniera de Software en Spotify</p>
-                <p className='testimony-text'>Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el 
-                que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba 
-                para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.</p>
+                <p className='testimony-name'>{props.name} en {props.country}</p>
+                <p className='testimony-position'>{props.position} en {props.company}</p>
+                <p className='testimony-text'>{props.testimony}</p>
             </div>
         </div>
     )
