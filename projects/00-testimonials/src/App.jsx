2c55306dab639263fe2cb2f12b545cf861f8c6b5
Grover Cristobal <dfrontend@medialabla.com>
2023-11-28 12:21:09 -0500

    feat(projects/00-testimonials): add responsive, color y comillas a textos

--- projects/00-testimonials/src/App.jsx
+++ projects/00-testimonials/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
                     image='/images/testimonio-shawn.png'
                     position='Ingeniero de Software'
                     company='Amazon'
-                    testimony='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'
+                    testimony='"Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida."'
                 />
 
                 <Testimony
@@ -24,7 +24,7 @@ function App() {
                     image='/images/testimonio-sarah.png'
                     position='Ingeniera de Software'
                     company='ChatDesk'
-                    testimony='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'
+                    testimony='"freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble."'
                 />
 
                 <Testimony
@@ -33,9 +33,9 @@ function App() {
                     image='/images/testimonio-emma.png'
                     position='Ingeniera de Software'
                     company='Spotify'
-                    testimony='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. 
+                    testimony='"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. 
                     Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para 
-                    conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
+                    conseguir el trabajo de mis sueños como ingeniero de software en Spotify."'
                 />
             </div>
         </div>
