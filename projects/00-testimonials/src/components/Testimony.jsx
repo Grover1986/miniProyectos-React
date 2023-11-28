0e22278c99e982dd66d6722fff9a3a568f9ac0be
Grover Cristobal <dfrontend@medialabla.com>
2023-11-28 15:25:38 -0500

    feat(projects/00-testimonials): Reto 2 - attr alt con sólo 1er nombre

--- projects/00-testimonials/src/components/Testimony.jsx
+++ projects/00-testimonials/src/components/Testimony.jsx
@@ -4,7 +4,7 @@ function Testimony(props) {
 
     return (
         <div className='testimony-container'>
-            <img className='testimony-image' src={props.image} alt={`Foto de ${props.name}`} />
+            <img className='testimony-image' src={props.image} alt={`Foto de ${props.name.charAt(0) + props.name.slice(1, props.name.indexOf(' '))}`} />
             <div className='text-testimony-container'>
                 <p className='testimony-name'><strong>{props.name}</strong> en {props.country}</p>
                 <p className='testimony-position'>{props.position} en <strong>{props.company}</strong></p>
