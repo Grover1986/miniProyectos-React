057a6d28d1b54cd10e722aefa35c7c623642fcfc
Grover Cristobal <dfrontend@medialabla.com>
2023-11-28 09:20:00 -0500

    feat(projects/00-testimonials): texto en negrita

--- projects/00-testimonials/src/components/Testimony.jsx
+++ projects/00-testimonials/src/components/Testimony.jsx
@@ -6,8 +6,8 @@ function Testimony(props) {
         <div className='testimony-container'>
             <img className='testimony-image' src={props.image} alt={`Foto de ${props.name}`} />
             <div className='text-testimony-container'>
-                <p className='testimony-name'>{props.name} en {props.country}</p>
-                <p className='testimony-position'>{props.position} en {props.company}</p>
+                <p className='testimony-name'><strong>{props.name}</strong> en {props.country}</p>
+                <p className='testimony-position'>{props.position} en <strong>{props.company}</strong></p>
                 <p className='testimony-text'>{props.testimony}</p>
             </div>
         </div>
