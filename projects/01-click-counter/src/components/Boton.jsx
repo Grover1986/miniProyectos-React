a02509ef2d1b6f03118cb32b839962baeed1cabc
Grover Cristobal <dfrontend@medialabla.com>
2023-12-02 01:40:00 -0500

    feat(projects/01-click-counter): asignar eventListener

--- projects/01-click-counter/src/components/Boton.jsx
+++ projects/01-click-counter/src/components/Boton.jsx
@@ -1,10 +1,14 @@
-function Boton ({ text, isButtonClic }) {
+function Boton ({ text, isButtonClic, handleClick }) {
 
     return (
         <button
-        className={`btn ${isButtonClic ? 'green' : 'lila'}`}> {/* agregamos una clase basada en condición para nuestro tipo de Botón, 'Clic' o 'Reiniciar' */}
+        className={`btn ${isButtonClic ? 'green' : 'lila'}`} // agregamos una clase basada en condición para nuestro tipo de Botón, 'Clic' o 'Reiniciar'
+        onClick={handleClick}> 
             {text}
         </button>
+        // aquí tenemos nuestro Boton con sus 3 props, uno para texto, otro para saber q tipo de Boton es, y el tercero que es una funcion para hacer clic
+        // la funcion handleClick va a venir del componente q renderice al Boton
+        // cuando hagamos clic en Boton vamos a tener que actualizar el estado de otro componente q es el Contador
     )
 }
 
