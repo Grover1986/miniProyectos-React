8d5381c2f3754f4fb224bcc456d5c922adc67b0b
Grover Cristobal <dfrontend@medialabla.com>
2023-12-02 01:10:00 -0500

    feat(projects/01-click-counter): clase basada en condición

--- projects/01-click-counter/src/components/Boton.jsx
+++ projects/01-click-counter/src/components/Boton.jsx
@@ -1,7 +1,8 @@
-function Boton ({ text }) {
+function Boton ({ text, isButtonClic }) {
 
     return (
-        <button>
+        <button
+        className={`btn ${isButtonClic ? 'green' : 'lila'}`}> {/* agregamos una clase basada en condición para nuestro tipo de Botón, 'Clic' o 'Reiniciar' */}
             {text}
         </button>
     )
