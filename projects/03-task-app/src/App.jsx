33ba39f73a3797696d6e29ddb2cf483a5d97c5c1
Grover Cristobal <dfrontend@medialabla.com>
2023-12-11 20:39:57 -0500

    feat(projects/03-task-app): estilos contenedor Lista de Tareas

--- projects/03-task-app/src/App.jsx
+++ projects/03-task-app/src/App.jsx
@@ -6,6 +6,10 @@ function App() {
    return (
       <div className='task-app'>
          <Logo />
+         <div className="main-task-list">
+            <h1>Mis Tareas</h1>
+            {/* aqui vamos a renderizar o crear lista de componentes es algo nuevo q no hemos visto */}
+         </div>
       </div>
    )
 }
