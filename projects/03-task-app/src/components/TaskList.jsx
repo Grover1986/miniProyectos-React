2f1ce7b1995a4f8b214a555b52522db53a1a0c1d
Grover Cristobal <dfrontend@medialabla.com>
2023-12-14 18:24:54 -0500

    feat(projects/03-task-app): estado de Lista de Tareas

--- projects/03-task-app/src/components/TaskList.jsx
+++ projects/03-task-app/src/components/TaskList.jsx
@@ -1,13 +1,15 @@
+import { useState } from 'react'
 import '../css/TaskList.css'
 import TaskForm from "./TaskForm"
 
 // este componente será el q contiene a ambos, tanto al formulario y a las tareas
 function TaskList() {
+
+    const [tasks, setTasks] = useState([])
+
     return (
-        // sino se quiere introducir un nuevo div q sea un contenedor, entonces se pueden usar los Fragmentos
         <>
             <TaskForm />
-            {/* dentro de este div tendremos una lista de componentes */}
             <div className='task-list-container'>
                 LISTA DE TAREAS
             </div>
