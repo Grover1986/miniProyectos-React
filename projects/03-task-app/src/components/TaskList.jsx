12b165580fbd4df4581cb96c0517b80bace549ae
Grover Cristobal <dfrontend@medialabla.com>
2023-12-14 16:27:30 -0500

    feat(projects/03-task-app): Fragmentos

--- projects/03-task-app/src/components/TaskList.jsx
+++ projects/03-task-app/src/components/TaskList.jsx
@@ -1,8 +1,16 @@
+import TaskForm from "./TaskForm"
 
 // este componente será el q contiene a ambos, tanto al formulario y a las tareas
 function TaskList() {
     return (
-
+        // sino se quiere introducir un nuevo div q sea un contenedor, entonces se pueden usar los Fragmentos
+        <>
+            <TaskForm />
+            {/* dentro de este div tendremos una lista de componentes */}
+            <div className='task-list-container'>
+                LISTA DE TAREAS
+            </div>
+        </>
     )
 }
 
