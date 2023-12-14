0727016a2ce323b9437d152cee64e79ca7db65b8
Grover Cristobal <dfrontend@medialabla.com>
2023-12-13 20:12:45 -0500

    feat(projects/03-task-app): instalando react-icons y dando estilos
    instalamos react-icons: npm install react-icons --save

--- projects/03-task-app/src/components/Task.jsx
+++ projects/03-task-app/src/components/Task.jsx
@@ -1,13 +1,14 @@
 import '../css/Task.css'
+import { AiOutlineCloseCircle } from 'react-icons/ai'; // importamos react-icons
 
 function Task({ text }) {
     return (
-        <div className="task-container">
-            <div className="task-text">
+        <div className='task-container'>
+            <div className='task-text'>
                 {text}
             </div>
-            <div className="task-icon">
-                Eliminar
+            <div className='task-icons-container'>
+                <AiOutlineCloseCircle className='task-icon' />  {/* aqui estamos asignando className como una prop, xq asi lo trabaja el componente */}
             </div>
         </div>
     )
