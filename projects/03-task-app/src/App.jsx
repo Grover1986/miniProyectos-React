a377b2031425f6163713e18ab4c55fd02171302a
Grover Cristobal <dfrontend@medialabla.com>
2023-12-14 16:09:09 -0500

    feat(projects/03-task-app): componente Lista de Tareas

--- projects/03-task-app/src/App.jsx
+++ projects/03-task-app/src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import Logo from './components/Logo'
-import Task from './components/Task'
 import TaskForm from './components/TaskForm';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
          <div className="main-task-list">
             <h1>Mis Tareas</h1>
             <TaskForm />
-            <Task text='Aprender React' />
          </div>
       </div>
    )
---
+++ projects/03-task-app/src/components/TaskList.jsx
@@ -0,0 +1,9 @@
+
+// este componente será el q contiene a ambos, tanto al formulario y a las tareas
+function TaskList() {
+    return (
+
+    )
+}
+
+export default TaskList
