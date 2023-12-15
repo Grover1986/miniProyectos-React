c18cbda8f869a27844790a174cc9e9c417a6bd64
Grover Cristobal <dfrontend@medialabla.com>
2023-12-14 19:18:12 -0500

    feat(projects/03-task-app): mostrar la Lista de Tareas

--- projects/03-task-app/src/components/TaskList.jsx
+++ projects/03-task-app/src/components/TaskList.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import '../css/TaskList.css'
 import TaskForm from "./TaskForm"
+import Task from './Task'
 
 // este componente será el q contiene a ambos, tanto al formulario y a las tareas
 function TaskList() {
@@ -11,7 +12,19 @@ function TaskList() {
         <>
             <TaskForm />
             <div className='task-list-container'>
-                LISTA DE TAREAS
+                {/* Ahora, ¿cómo mostrar una lista de componentes de React? 
+                    * ps vamos a generar una lista de componentes Tarea (Task) y cada uno de esos componentes va a tener sus propios valores
+                    * Pare renderizar una lista de componentes escribimos {} xq vamos a necesitar un método de arreglos de js
+                */}
+                {
+                    // cada task se va a representar como un Objeto en el Array y para cada uno de ellos se va a crear un componente
+                    tasks.map((task) => 
+                        <Task 
+                            text={task.text}
+                            completed={task.completed}
+                        />
+                    )
+                }
             </div>
         </>
     )
