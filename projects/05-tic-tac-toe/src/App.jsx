b14cf6cb8cb421cf36e817d43d52398c2369dd5a
Grover Cristobal <elvis.cristobal.garrido@gmail.com>
2024-07-28 06:53:55 -0500

    feat(projects/05-tic-tac-toe): dibujando el tablero2

--- projects/05-tic-tac-toe/src/App.jsx
+++ projects/05-tic-tac-toe/src/App.jsx
@@ -6,24 +6,33 @@ const TURNS = {
    O: 'o'
 }
 
-// variable tablero y su creación de índices
-const board = Array(9).fill(null)
+// Como haremos un tablero para jugar tres en raya (michi), tenemos que componetizar y ¿Qué es lo q compone un Tablero?, pues un Cuadrado q va a ser la posición
+// donde se va a poder jugar cada uno de los movimientos. Así q creamos nuestro componente Square q necesitará un children, una actualización y un índice
+const Square = ({ children, updateBoard, index }) => {
+   return (
+      <div className='square'>
+         {children}
+      </div>
+   )
+}
 
 function App() {
-
+   const board = Array(9).fill(null)
+   
    return (
-      <main className='board'>
+      <main className='board'>   
          <h1>Tic Tac Toe</h1>
          <section className='game'>
             {
                // llenamos nuestro tablero colocando como valor sus índices
                board.map((_, index) => {
                   return (
-                     <div key={index}>
-                        <span>
-                           {index}
-                        </span>
-                     </div>
+                     <Square
+                        key={index}
+                        index={index}
+                     >
+                        {index}
+                     </Square>
                   )
                })
             }
