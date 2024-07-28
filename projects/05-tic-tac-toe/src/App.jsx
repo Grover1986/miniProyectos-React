a0cda6157e98a1fbaa15ca76f9b91f277df18c30
Grover Cristobal <elvis.cristobal.garrido@gmail.com>
2024-07-28 07:35:10 -0500

    feat(projects/05-tic-tac-toe): estado inicial y estado turnos

--- projects/05-tic-tac-toe/src/App.jsx
+++ projects/05-tic-tac-toe/src/App.jsx
@@ -1,4 +1,4 @@
-/* Juego de tres en raya Tic Tac Toe (michi) */
+import { useState } from 'react'
 
 // objeto Turnos
 const TURNS = {
@@ -8,16 +8,22 @@ const TURNS = {
 
 // Como haremos un tablero para jugar tres en raya (michi), tenemos que componetizar y ¿Qué es lo q compone un Tablero?, pues un Cuadrado q va a ser la posición
 // donde se va a poder jugar cada uno de los movimientos. Así q creamos nuestro componente Square q necesitará un children, una actualización y un índice
-const Square = ({ children, updateBoard, index }) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
+   const className = `square ${isSelected ? 'is-selected': ''}`
    return (
-      <div className='square'>
+      <div className={className}>
          {children}
       </div>
    )
 }
 
 function App() {
-   const board = Array(9).fill(null)
+   /* Vamos a usar un estado para guardar cuando el usuario hace click en cada posición, y no sólo existen estados de true o false, sino tmb más complejos; en este caso 
+   cuando demos click a cada Square (cuadrado) vamos a tener q actualizar el tablero para saber si se ha puesto una X o no */
+   const [board, setBoard] = useState(Array(9).fill(null))
+
+   // estado con los turnos
+   const [turn, setTurn] = useState(TURNS.X)
    
    return (
       <main className='board'>   
@@ -36,6 +42,16 @@ function App() {
                   )
                })
             }
+         </section>
+
+         {/* Ahora, necesitamos saber visualmente cual es el turno, así q eso lo hacemos aquí: */}
+         <section className='turn'>
+            <Square isSelected = {turn === TURNS.X}>
+               {TURNS.X}
+            </Square>
+            <Square isSelected = {turn === TURNS.O}>
+               {TURNS.O}
+            </Square>
          </section>
       </main>
    )
