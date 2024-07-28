3eb6e9dc64b2410a2e4d74517f8a5dce3f7312d7
Grover Cristobal <elvis.cristobal.garrido@gmail.com>
2024-07-28 06:05:32 -0500

    feat(projects/05-tic-tac-toe): dibujando el tablero

--- projects/05-tic-tac-toe/src/App.jsx
+++ projects/05-tic-tac-toe/src/App.jsx
@@ -1,9 +1,35 @@
 function App() {
+   
+   /* Juego de tres en raya Tic Tac Toe (michi) */
 
-  return (
-    <>
-    </>
-  )
+   // objeto Turnos
+   const TURNS = {
+      X: 'x',
+      O: 'o'
+   }
+
+   // variable tablero y su creación de índices
+   const board = Array(9).fill(null)
+
+   return (
+      <main className='board'>
+         <h1>Tic Tac Toe</h1>
+         <section className='game'>
+            {
+               // llenamos nuestro tablero colocando como valor sus índices
+               board.map((_, index) => {
+                  return (
+                     <div key={index}>
+                        <span>
+                           {index}
+                        </span>
+                     </div>
+                  )
+               })
+            }
+         </section>
+      </main>
+   )
 }
 
 export default App
